08574666274799f0c9a77b9f9b557377453f07e8
Jerry.Shi <skirrund@hotmail.com>
2022-12-13 01:55:56 +0800

    cors remove

--- plugins/server/http/gin/gin_server.go
+++ plugins/server/http/gin/gin_server.go
@@ -63,7 +63,7 @@ func NewServer(options server.Options, routerProvider func(engine *gin.Engine), 
 		c.JSON(200, response.Fail(fmt.Sprintf("%v", recovered)))
 		//		c.AbortWithStatus(http.StatusInternalServerError)
 	}))
-	s.Use(cors)
+	//s.Use(cors)
 	s.Use(zipkinMiddleware)
 	s.Use(loggingMiddleware)
 	//zipkin.InitZipkinTracer(s)
@@ -87,28 +87,28 @@ func initSwagger(e *gin.Engine) {
 	e.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 }
 
-func cors(c *gin.Context) {
-	method := c.Request.Method
-	//origin := c.Request.Header.Get("Origin")
-	// //接收客户端发送的origin （重要！）
-	// header := c.Writer.Header()
-	// header.Set("Access-Control-Allow-Origin", "*")
-	// //服务器支持的所有跨域请求的方法
-	// header.Set("Access-Control-Allow-Methods", "*")
-	// //允许跨域设置可以返回其他子段，可以自定义字段
-	// c.Header("Access-Control-Allow-Headers", "*")
-	// // 允许浏览器（客户端）可以解析的头部 （重要）
-	// //c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
-	// //设置缓存时间
-	// //	c.Header("Access-Control-Max-Age", "172800")
-	// //允许客户端传递校验信息比如 cookie (重要)
-	// c.Header("Access-Control-Allow-Credentials", "true")
-	if method == "OPTIONS" {
-		c.AbortWithStatus(http.StatusNoContent)
-	} else {
-		c.Next()
-	}
-}
+// func cors(c *gin.Context) {
+// 	method := c.Request.Method
+//origin := c.Request.Header.Get("Origin")
+// //接收客户端发送的origin （重要！）
+// header := c.Writer.Header()
+// header.Set("Access-Control-Allow-Origin", "*")
+// //服务器支持的所有跨域请求的方法
+// header.Set("Access-Control-Allow-Methods", "*")
+// //允许跨域设置可以返回其他子段，可以自定义字段
+// c.Header("Access-Control-Allow-Headers", "*")
+// // 允许浏览器（客户端）可以解析的头部 （重要）
+// //c.Header("Access-Control-Expose-Headers", "Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers")
+// //设置缓存时间
+// //	c.Header("Access-Control-Max-Age", "172800")
+// //允许客户端传递校验信息比如 cookie (重要)
+// c.Header("Access-Control-Allow-Credentials", "true")
+// 	if method == "OPTIONS" {
+// 		c.AbortWithStatus(http.StatusNoContent)
+// 	} else {
+// 		c.Next()
+// 	}
+// }
 
 // func sentinelMiddleware(c *gin.Context) {
 // 	var args []interface{}
