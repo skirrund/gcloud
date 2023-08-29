cc11fcc206f718cf887bd05d8e2d78de06c1d2a9
Jerry.Shi <>
2023-08-29 18:11:45 +0800

    可控并发数量执行

--- utils/worker/worker.go
+++ utils/worker/worker.go
@@ -1,9 +1,53 @@
 package worker
 
 import (
-	"github.com/skirrund/gcloud/logger"
 	"runtime/debug"
+
+	"github.com/skirrund/gcloud/logger"
 )
+
+type Worker struct {
+	quene chan struct{}
+	Limit uint64
+}
+
+var DefaultWorker Worker
+
+const (
+	DefaultLimit uint64 = 512
+)
+
+func init() {
+	DefaultWorker = Worker{
+		quene: make(chan struct{}, DefaultLimit),
+		Limit: DefaultLimit,
+	}
+}
+
+func Init(limit uint64) Worker {
+	return Worker{
+		quene: make(chan struct{}, limit),
+		Limit: limit,
+	}
+}
+func (w Worker) Execute(f func()) {
+	w.quene <- struct{}{}
+	go func() {
+		defer func() {
+			<-w.quene
+		}()
+		execute(f)
+	}()
+}
+
+func execute(f func()) {
+	defer func() {
+		if err := recover(); err != nil {
+			logger.Error("[worker] error recover :", err, "\n", string(debug.Stack()))
+		}
+	}()
+	f()
+}
 
 func AsyncExecute(f func()) {
 	go func() {
--- utils/worker/worker_test.go
+++ utils/worker/worker_test.go
@@ -2,28 +2,17 @@ package worker
 
 import (
 	"fmt"
-	"sync"
 	"testing"
 )
 
-var wg sync.WaitGroup
-
 func TestAsyncExecute(t *testing.T) {
-	a, b, c, d := 1, 2, 3, 4
-	ids := []*int{&a, &b, &c, &d}
-	for i := range ids {
-		wg.Add(1)
-		//wg.Add(1)
-		x := ids[i]
-		AsyncExecute(func() {
-			demo(*x)
+	for i := 0; i < 1000; i++ {
+		i1 := i
+		fmt.Println("send:", i1)
+		DefaultWorker.Execute(func() {
+			fmt.Println(i1)
+
 		})
 	}
-	wg.Wait()
 	println("done")
-}
-
-func demo(x int) {
-	wg.Done()
-	fmt.Println(x)
 }
