3b6ec2a2502f80224fb00c3fb6353e50b8528663
Jerry.Shi <>
2023-09-09 09:43:36 +0800

    cookie

---
+++ server/http/cookie.go
@@ -0,0 +1,29 @@
+package http
+
+import (
+	"time"
+)
+
+type CookieSameSite string
+
+const (
+	// CookieSameSiteLaxMode sets the SameSite flag with the "Lax" parameter
+	CookieSameSiteLaxMode = "Lax"
+	// CookieSameSiteStrictMode sets the SameSite flag with the "Strict" parameter
+	CookieSameSiteStrictMode = "Strict"
+	// CookieSameSiteNoneMode sets the SameSite flag with the "None" parameter
+	// see https://tools.ietf.org/html/draft-west-cookie-incrementalism-00
+	CookieSameSiteNoneMode = "None"
+)
+
+type Cookie struct {
+	Key      string
+	Value    string
+	Expire   time.Time
+	MaxAge   int
+	Domain   string
+	Path     string
+	HttpOnly bool
+	Secure   bool
+	SameSite CookieSameSite
+}
