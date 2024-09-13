49b91bca58c64c28aa6354406e47a9f307e1fab0
Jerry.Shi <>
2024-09-13 12:17:51 +0800

    判断重复key

--- database/dialectors/gmysql/mysql_dialector.go
+++ database/dialectors/gmysql/mysql_dialector.go
@@ -1,6 +1,7 @@
 package gmysql
 
 import (
+	mysql1 "github.com/go-sql-driver/mysql"
 	"github.com/skirrund/gcloud/database/option"
 	"gorm.io/driver/mysql"
 	"gorm.io/gorm"
@@ -16,4 +17,14 @@ func (MysqlDialector) New(option option.Option) gorm.Dialector {
 	return mysql.New(mysql.Config{
 		DSN: option.DSN,
 	})
+}
+
+func IsDuplicateKeyError(err error) bool {
+	if err == nil {
+		return false
+	}
+	if merr, ok := err.(*mysql1.MySQLError); ok {
+		return merr.Number == 1062
+	}
+	return false
 }
