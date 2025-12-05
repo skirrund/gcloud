30b337cd7e1e20d6a9a4306a229cd8b8185c3c4b
Jerry.Shi <skirrund@hotmail.com>
2025-12-05 13:20:30 +0800

    utils

--- plugins/server/http/gin/utils.go
+++ plugins/server/http/gin/utils.go
@@ -2,6 +2,7 @@ package gin
 
 import (
 	"errors"
+	"reflect"
 
 	"github.com/skirrund/gcloud/response"
 	"github.com/skirrund/gcloud/utils/validator"
@@ -9,6 +10,7 @@ import (
 	"strings"
 
 	"github.com/gin-gonic/gin"
+	"github.com/gin-gonic/gin/binding"
 )
 
 func CheckQueryParamsWithErrorMsg(name string, v *string, errorMsg string, ctx *gin.Context) bool {
@@ -59,12 +61,57 @@ func SendJSON(ctx *gin.Context, data any) {
 	ctx.JSON(200, data)
 }
 
-func ShouldBind(ctx *gin.Context, data any) error {
-	return ctx.ShouldBind(data)
+// 优先级
+// path > form > query > cookie > header > json > raw_body
+func ShouldBind(ctx *gin.Context, obj any) (err error) {
+	if obj == nil {
+		return nil
+	}
+	t := reflect.TypeOf(obj)
+	kind := t.Kind()
+	if kind == reflect.Pointer {
+		t = t.Elem()
+	} else {
+		return nil
+	}
+	kind = t.Kind()
+	var tagHeader, tagQuery, tagPath, tagForm bool
+	if kind == reflect.Struct {
+		nfs := t.NumField()
+		for i := range nfs {
+			sf := t.Field(i)
+			if len(sf.Tag.Get("header")) > 0 {
+				tagHeader = true
+			}
+			if len(sf.Tag.Get("query")) > 0 {
+				tagQuery = true
+			}
+			if len(sf.Tag.Get("form")) > 0 {
+				tagForm = true
+			}
+			if len(sf.Tag.Get("path")) > 0 {
+				tagPath = true
+			}
+		}
+	}
+	if err = ctx.ShouldBind(obj); err != nil {
+		return err
+	}
+	if tagHeader {
+		ctx.ShouldBindHeader(obj)
+	}
+	if tagQuery || tagForm {
+		ctx.ShouldBindWith(obj, binding.Query)
+		ctx.ShouldBindWith(obj, binding.Form)
+	}
+	if tagPath {
+		ctx.ShouldBindUri(obj)
+	}
+	return nil
 }
 
 func ShouldBindAndValidate(ctx *gin.Context, data any) error {
-	err := ctx.ShouldBind(data)
+	err := ShouldBind(ctx, data)
 	if err != nil {
 		return err
 	}
